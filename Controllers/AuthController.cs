e11a77ce08047f619b682f79718b95a4783162d5
daveudith@gmail.com <daveudith@gmail.com>
2026-06-12 23:34:02 +0530

    Initialize AuthController class and start develop logic phase

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
+
+namespace BankAuthApi.DTOs;
+
 [ApiController]
-[]
+[Route("api/auth")]
 public class AuthController : ControllerBase
 {
-    
+    [HttpPost("register")]
+    public IActionResult Register(RegisterDTO registerDTO)
+    {
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginDTO loginDTO)
+    {
+        return Ok();
+    }
 }
